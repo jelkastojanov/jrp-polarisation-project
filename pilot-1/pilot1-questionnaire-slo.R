7f7b2b200e59992ca9c7a45c03dd90636833caec
Ziga Mekis Recek <ziga.mekisrecek@gmail.com>
2022-02-10 17:13:37 +0100

    merging1-quest.

--- pilot-1/pilot1-questionnaire-slo.R
+++ pilot-1/pilot1-questionnaire-slo.R
@@ -1,5 +1,14 @@
 party<- read.delim2(pipe("pbpaste"))
-
+#merging all questinnaires, which were "cleaned" in excel
 quest1 <- merge(demografija, party, by="Participant.Public.ID", all=TRUE)
 total_ques<- merge(quest1, libertarism, by="Participant.Public.ID", all=TRUE)
+
+names(total_ques$Participant.Public.ID)<-c("ID")
+total_ques$ID<-total_ques$Participant.Public.ID
+total_ques
+
+#merging data from the tasks and the quest.
+data_pilot1<- merge(total_ques, data_task, by="ID", all=TRUE)
+View(data_pilot1)
+
 
